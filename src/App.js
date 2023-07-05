868506bc70c0c760b9326f8fd8495dff6524bfe3
A-Tech-Services <abiodunoluwapelumi859@gmail.com>
2023-07-05 12:35:43 +0100

    new react project

--- src/App.js
+++ src/App.js
@@ -1,25 +1,46 @@
-import logo from './logo.svg';
+import { useEffect } from 'react';
 import './App.css';
+import SearchIcon from './search.svg';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+// declaring a public variable which connect to the omdbapi server 
+// with my api key
+const API_URL = 'http://www.omdbapi.com?apikey=9703997b'
+
+
+// This my App function component.
+const App = () => {
+    const SearchMovies = async (title) => {
+        const response = await fetch(`${API_URL}&s=${title}`);
+        const data =await response.json();
+
+        console.log(data.Search);
+    }
+
+    useEffect(() => {
+        SearchMovies('Anikulapo');
+    }, []);
+
+    // App display
+    return(
+        <div className='app'>
+            <h1>Movie-Land</h1>
+
+            {/* search bar container below */}
+            <div className='search'>
+                <input 
+                    placeholder='Search for movies . . .' 
+                    value='Superman' 
+                    onChange={() => {}}
+                />
+
+                <img
+                    src={SearchIcon}
+                    alt='Search Icon'
+                />
+            </div>
+        </div>
+    );
 }
 
 export default App;
